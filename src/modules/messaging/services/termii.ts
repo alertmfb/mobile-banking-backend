f5d58626b349fe42627ab6c3c1c81263266ffed5
Olumide Daniel <danieloluboies@gmail.com>
2025-01-30 11:05:12 +0100

    message

--- src/modules/messaging/services/termii.ts
+++ src/modules/messaging/services/termii.ts
@@ -69,21 +69,21 @@ export class Termii implements MessagingService {
   }
 
   async sendWhatsapp(phone: string, message: string) {
-    console.log('sending whatsapp', { phone, message });
     try {
-      const response = await lastValueFrom(
-        this.httpService.post(
-          `${this.baseUrl}/broadcast/whatsapp`,
-          {
-            to: phone,
-            sms: message,
-          },
-          {
-            headers: this.header,
-          },
-        ),
-      );
-      return response.data;
+      // const response = await lastValueFrom(
+      //   this.httpService.post(
+      //     `${this.baseUrl}/broadcast/whatsapp`,
+      //     {
+      //       to: phone,
+      //       sms: message,
+      //     },
+      //     {
+      //       headers: this.header,
+      //     },
+      //   ),
+      // );
+      // return response.data;
+      return { phone, message };
     } catch (error) {
       throw new HttpException(
         `Messaging Error: ${error?.response?.data?.message}`,
