31ab760d35d88a6991460df52bc52b84b8cf9d22
Daniel Olumide <danieloluboies@gmail.com>
2025-02-12 16:05:23 +0100

    bill payment

--- src/modules/bill-payment/dtos/credit-switch/transform-airtime-data-providers.dto.ts
+++ src/modules/bill-payment/dtos/credit-switch/transform-airtime-data-providers.dto.ts
@@ -16,10 +16,19 @@ export class CreditSwitchAirtimeTransferResponseDto {
             ? 'NINE_MOBILE'
             : formattedIdentifier;
 
+      const logoObj = {
+        MTN: 'https://res.cloudinary.com/dcm6r6nv7/image/upload/v1739372524/mtn_eudpji.png',
+        GLO: 'https://res.cloudinary.com/dcm6r6nv7/image/upload/v1739372524/glo_ta8ya8.png',
+        NINE_MOBILE:
+          'https://res.cloudinary.com/dcm6r6nv7/image/upload/v1739372524/9-mobile_qjyndd.png',
+        AIRTEL:
+          'https://res.cloudinary.com/dcm6r6nv7/image/upload/v1739372524/airtel_pzriyg.png',
+      };
+
       return {
         serviceCategoryId,
         name,
-        logo: `https://example.com/logos/${name.toLowerCase().replace(/\s+/g, '-')}.png`,
+        logo: logoObj[identifier],
         identifier,
       };
     });
