92a1090054e71565a16397f6988e028dcf50509c
Daniel Olumide <danieloluboies@gmail.com>
2025-02-12 15:59:12 +0100

    bill payment

--- src/modules/bill-payment/dtos/credit-switch/transform-airtime-data-providers.dto.ts
+++ src/modules/bill-payment/dtos/credit-switch/transform-airtime-data-providers.dto.ts
@@ -1,15 +1,27 @@
 export class CreditSwitchAirtimeTransferResponseDto {
-    serviceCategoryId: string;
-    name: string;
-    logo: string;
-    identifier: string;
+  serviceCategoryId: string;
+  name: string;
+  logo: string;
+  identifier: string;
 
-    static fromObject(data: Record<string, string>): CreditSwitchAirtimeTransferResponseDto[] {
-        return Object.entries(data).map(([name, serviceCategoryId]) => ({
-            serviceCategoryId,
-            name,
-            logo: `https://example.com/logos/${name.toLowerCase().replace(/\s+/g, '-')}.png`,
-            identifier: name.toUpperCase().replace(/\s+/g, '_')
-        }));
-    }
+  static fromObject(
+    data: Record<string, string>,
+  ): CreditSwitchAirtimeTransferResponseDto[] {
+    return Object.entries(data).map(([name, serviceCategoryId]) => {
+      const formattedIdentifier = name.toUpperCase().replace(/\s+/g, '_');
+      const identifier =
+        formattedIdentifier === 'GLOBACOM'
+          ? 'GLO'
+          : formattedIdentifier === '9_MOBILE'
+            ? 'NINE_MOBILE'
+            : formattedIdentifier;
+
+      return {
+        serviceCategoryId,
+        name,
+        logo: `https://example.com/logos/${name.toLowerCase().replace(/\s+/g, '-')}.png`,
+        identifier,
+      };
+    });
+  }
 }
